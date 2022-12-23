739fc5fe3de3312fd2c391e9fabc23f8c5055b41
Hoang Anh Tuan <hoangtuandhbk@gmail.com>
2022-12-23 16:40:04 +0700

    ...

---
+++ scripts/17.getListOfFullZones.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Get China Network Enabled Zones
+ *
+ * @author: tuanha
+ */
+require(__DIR__.'/../bootstrap.php');
+
+$zoneMgmt = new CFBuddy\ZoneMgmt();
+$fullZones = [];
+
+$page = 1;
+do {
+    $zones = $zoneMgmt->getZones($page, 1000);
+    if (empty($zones)) {
+        print "No more zone to proceed \n";
+        break;
+    }
+    $data = array_filter($zones, function ($zone) {
+        return isset($zone['type']) && $zone['type'] != 'partial';
+     });
+    if (!empty($data)) {
+        $data = array_merge([], array_map(function ($zone) {
+            return $zone['name'];
+        }, $data));
+    }
+    $fullZones = array_merge($fullZones, $data);
+    print "Proceeded page " . $page . "\n";
+    ++$page;
+} while (!empty($zones));
+
+file_put_contents(__DIR__ . '/../output/fullZones.txt', implode(",", $fullZones));
